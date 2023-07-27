b54d2b3d72ff55db18867c0686be95df34f73a1a
emmanuel.lima <86535005+Emmanuel194@users.noreply.github.com>
2023-07-27 09:36:37 -0300

    second commit

---
+++ aula 25-07/exer10/index.js
@@ -0,0 +1,12 @@
+const prompt = require ('prompt-sync')();
+
+
+function converter () {
+ let graus = prompt ("escreva um número em graus celsius : ");
+ if (graus + 1.8* + 32);
+ console.log("pronto, agr está transformado em Fahrenheit");
+
+}
+
+
+converter();
