cc62768de0a07f89c7bb77b9bb8aff938190ce42
emmanuel.lima <86535005+Emmanuel194@users.noreply.github.com>
2023-08-24 01:12:00 -0300

    commit fist

--- 02 -Javascript-orientação-objetos/aula 22-08/index.js
+++ 02 -Javascript-orientação-objetos/aula 22-08/index.js
@@ -1,14 +1,56 @@
-const button = document.getElementById("botao1");
-const input = document.getElementById("nome");
-const inputSenha = document.getElementById("senha");
+const button = document.querySelector(".button1");
+const input = document.querySelector("form .nome");
+const inputSobrenome = document.querySelector("form .sobrenome")
+var minhaDiv = document.querySelector(".lista")
+var alunos = [];
 
 
-console.log(button,nome,senha);
-
-button.addEventListener("click", lidarComCliker);
-
-
-function lidarComCliker(evento){
- const valorDoCampo = input.value;
- alert(valorDoCampo);
+function nomeSobrenome (input, inputSobrenome){
+  return {
+    input,inputSobrenome
+  }
 }
+
+
+button.addEventListener("click",function (e) {
+    e.preventDefault()
+    if(typeof input.value === "string") {
+      alunos.push(nomeSobrenome(input.value,inputSobrenome.value))
+      
+      console.log(alunos)
+    }
+    for (var i = 0; i < alunos.length; i++) {
+      var listaAlunos = alunos[i];
+      let p = document.createElement("p")
+      let botao = document.createElement("button")
+      botao.textContent = "apagar"
+      botao.setAttribute ("class","deletar")
+      p.textContent = `${listaAlunos.input}${listaAlunos.inputSobrenome} `;
+      p.appendChild (botao)
+      minhaDiv.appendChild(p)
+      
+      console.log(listaAlunos)
+
+    }
+     
+}) 
+
+document.addEventListener("click", function (event){
+  const clickUser = event.target;
+  if (clickUser.classList.contains("deletar")){
+    clickUser.parentElement.remove();
+    console.log(clickUser);
+  }
+  
+})
+
+
+
+ 
+
+
+
+
+
+
+
