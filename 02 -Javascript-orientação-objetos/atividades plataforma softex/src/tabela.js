5fa9d0c3b7770744933c5bb921224ecb1f6fab97
emmanuel.lima <86535005+Emmanuel194@users.noreply.github.com>
2023-09-15 20:20:58 -0300

    commit

---
+++ 02 -Javascript-orientação-objetos/atividades plataforma softex/src/tabela.js
@@ -0,0 +1,38 @@
+const pessoas = [
+  {
+    nome: "jose",
+    idade: 19,
+    salario: 1900,
+    temEnsinoMedioCompleto: true
+  },
+  {
+    nome: "david",
+    idade: 22,
+    salario: 2500,
+    temEnsinoMedioCompleto: false
+  },
+  {
+    nome: "maria",
+    idade: 25,
+    salario: 3400,
+    temEnsinoMedioCompleto: true
+  },
+  {
+    nome: "igor",
+    idade: 18,
+    salario: 2100,
+    temEnsinoMedioCompleto: false
+  }
+];
+
+
+console.log("Nome:", pessoas[0].nome);
+console.log("Idade:", pessoas[0].idade);
+console.log("Salário:", pessoas[0].salario);
+console.log("Tem Ensino Médio Completo?", pessoas[0].temEnsinoMedioCompleto);
+
+
+console.log("Nome:", pessoas[1].nome);
+console.log("Idade:", pessoas[1].idade);
+console.log("Salário:", pessoas[1].salario);
+console.log("Tem Ensino Médio Completo?", pessoas[1].temEnsinoMedioCompleto);
