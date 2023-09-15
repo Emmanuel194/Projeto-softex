5b36942bb681f15f13f722342502511656eb5d80
emmanuel.lima <86535005+Emmanuel194@users.noreply.github.com>
2023-09-15 20:44:55 -0300

    commit

---
+++ 02 -Javascript-orientação-objetos/atividades plataforma softex/src/manipulando.js
@@ -0,0 +1,11 @@
+//dois métodos comuns usados para manipular strings em JavaScript são:
+
+concat();
+
+toUpperCase();
+
+var string1 = "Olá, ";
+var string2 = "mundo!";
+var resultado = string1.concat(string2);
+
+console.log(resultado); // Saída: "Olá, mundo!
